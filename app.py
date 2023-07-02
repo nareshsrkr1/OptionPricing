cdc04f9ae97e410392b103a8db88a970fa9e284b
rahlu <prog.rahul01@gmail.com>
2023-07-02 16:24:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,47 @@ books = [
 @app.route('/books', methods=['GET'])
 def get_books():
     return jsonify(books)
+
+# GET /books/<book_id> - Get a specific book
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if book:
+        return jsonify(book)
+    else:
+        return jsonify({'error': 'Book not found'}), 404
+
+# POST /books - Create a new book
+@app.route('/books', methods=['POST'])
+def create_book():
+    new_book = {
+        'id': len(books) + 1,
+        'title': request.json['title'],
+        'author': request.json['author']
+    }
+    books.append(new_book)
+    return jsonify(new_book), 201
+
+# PUT /books/<book_id> - Update a book
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if book:
+        book['title'] = request.json.get('title', book['title'])
+        book['author'] = request.json.get('author', book['author'])
+        return jsonify(book)
+    else:
+        return jsonify({'error': 'Book not found'}), 404
+
+# DELETE /books/<book_id> - Delete a book
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if book:
+        books.remove(book)
+        return jsonify({'message': 'Book deleted'})
+    else:
+        return jsonify({'error': 'Book not found'}), 404
+
+if __name__ == '__main__':
+    app.run()
