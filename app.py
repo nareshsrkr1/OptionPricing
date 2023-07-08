90dd5591ac8751893ac8ddc0d33ebffbfde1600b
rahlu <prog.rahul01@gmail.com>
2023-07-09 00:12:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 books = [
     {"id": 1, "title": "Book 1", "author": "Author 1"},
     {"id": 2, "title": "Book 2", "author": "Author 2"},
-    {"id": 3, "title": "Book 3", "author": "Author 3"},
-    {"id": 4, "title": "Book 3", "author": "Author 4"}
+    {"id": 3, "title": "Book 3", "author": "Author 3"}
 ]
 
 # GET /books - Get all books
